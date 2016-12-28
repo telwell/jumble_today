d582bd362f1d21227aa09400ef2c9e05ffc43b6e
telwell <me@trevorelwell.com>
2016-12-27 21:20:22 -0500

    gitignore and tabs->spaces

--- app/controllers/dictionary_controller.rb
+++ app/controllers/dictionary_controller.rb
@@ -1,11 +1,11 @@
 class DictionaryController < ApplicationController
 
 
-	def index
-		# http://www.setgetgo.com/randomword/ thank you!!
-		# http://developer.wordnik.com/docs.html#!/words/getRandomWords_get_3
-		@words = HTTParty.get("http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&excludePartOfSpeech=idiom,proper-noun-plural,proper-noun-posessive&minCorpusCount=600&maxCorpusCount=-1&minDictionaryCount=100&minLength=5&maxLength=5&limit=5&api_key=#{Rails.application.secrets.wordnik_key}")
-	end
+  def index
+    # http://www.setgetgo.com/randomword/ thank you!!
+    # http://developer.wordnik.com/docs.html#!/words/getRandomWords_get_3
+    @words = HTTParty.get("http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&excludePartOfSpeech=idiom,proper-noun-plural,proper-noun-posessive&minCorpusCount=600&maxCorpusCount=-1&minDictionaryCount=100&minLength=5&maxLength=5&limit=5&api_key=#{Rails.application.secrets.wordnik_key}")
+  end
 
 
 end
