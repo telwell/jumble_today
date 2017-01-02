4c2090e19316688a7600caab94aa1229c5658d03
telwell <me@trevorelwell.com>
2017-01-02 17:13:29 -0500

    ordering leaderboard

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,7 +1,10 @@
 class LeaderboardController < ApplicationController
 
 	def index
-		@users = User.all
+		@users = User.joins(:user_jumbles)
+			.select(:id, :username, "count(user_id) as count")
+			.group(:user_id, :id)
+			.order("count desc", :created_at)
 	end
 
 end
